1e53088168ff5d1cdeff01738964e8bd7652780f
Isaac Flemming <isaacflemming@Isaacs-Mac-mini.local>
2014-05-04 17:43:25 -0700

    added inventory pick up, doors and keys

--- week1/scripts/dungeon.js
+++ week1/scripts/dungeon.js
@@ -1,29 +1,180 @@
 var dungeon,
     header,
-    footer;
+    footer,
+    maxItems;
 
 var rare_item_types = []
 
-var normal_item_types = []
+var normal_item_types = [
+    {
+        key: 'ch',
+        sprite: 'ch',
+        type: 'cloth-hat',
+        description: 'keeps your brain warm',
+        attributes: {
+            armor: 0,
+            damage: 0,
+            weight: 0,
+            warm: 1
+        }
+    },{
+        key: 'cs',
+        sprite: 'cs',
+        type: 'cloth-shirt',
+        description: 'keeps you warm and in style',
+        attributes: {
+            armor: 0,
+            damage: 0,
+            weight: 0,
+            warm: 1
+        }
+    },{
+        key: 'cp',
+        sprite: 'cp',
+        type: 'cloth-pants',
+        description: 'keeps your legs warm',
+        attributes: {
+            armor: 0,
+            damage: 0,
+            weight: 0,
+            warm: 1
+        }
+    },{
+        key: 'lh',
+        sprite: 'lh',
+        type: 'leather-hat',
+        description: 'keeps your brain safe',
+        attributes: {
+            armor: 1,
+            damage: 0,
+            weight: 0,
+            warm: 1
+        }
+    },{
+        key: 'ls',
+        sprite: 'ls',
+        type: 'leather-shirt',
+        description: 'keeps you warm and safe',
+        attributes: {
+            armor: 1,
+            damage: 0,
+            weight: 1,
+            warm: 1
+        }
+    },{
+        key: 'lp',
+        sprite: 'lp',
+        type: 'leather-pants',
+        description: 'keeps your legs warm',
+        attributes: {
+            armor: 1,
+            damage: 0,
+            weight: 1,
+            warm: 1
+        }
+    },{
+        key: 'mh',
+        sprite: 'mh',
+        type: 'metal-hat',
+        description: 'a sturdy metal helmet',
+        attributes: {
+            armor: 3,
+            damage: 0,
+            weight: 2,
+            warm: 1
+        }
+    },{
+        key: 'ms',
+        sprite: 'ms',
+        type: 'mail-shirt',
+        description: 'chain mail shirt',
+        attributes: {
+            armor: 3,
+            damage: 0,
+            weight: 2,
+            warm: 1
+        }
+    },{
+        key: 'mp',
+        sprite: 'mp',
+        type: 'mail-pants',
+        description: 'chain mail pants',
+        attributes: {
+            armor: 3,
+            damage: 0,
+            weight: 2,
+            warm: 1
+        }
+    },{
+        key: 'ws',
+        sprint: 'ws',
+        type: 'wooden-sword',
+        description: 'a wooden sword, who are we kidding, call it a stick.',
+        attributes: {
+            armor: 0,
+            damage: 1,
+            weight: 1,
+            warm: 0
+        }
+    },{
+        key: 'is',
+        sprite: 'is',
+        type: 'iron-sword',
+        description: 'a real sword, made out of metal and everything.',
+        attributes: {
+            armor: 0,
+            damage: 3,
+            weight: 2,
+            warm: 0
+        }
+    },{
+        key: 'sd',
+        sprite: 'sd',
+        type: 'steel-dagger',
+        description: 'a small, sharp and hard dagger',
+        attributes: {
+            armor: 0,
+            damage: 2,
+            weight: 0,
+            warm: 0
+        }
+    },{
+        key: 'wc',
+        sprite: 'wc',
+        type: 'wooden-club',
+        description: 'a bigger stick',
+        attributes: {
+            armor: 0,
+            damage: 2,
+            weight: 2,
+            warm: 0
+        }
+    }
+]
 
 var ground_item_types = [
     {
+        key: 'k',
         sprite: 'k',
         type: 'key',
         description: 'a key'
     },{
+        key: 'g',
         sprite: 'g',
         type: 'small-gold',
         description: 'a small gold coin'
     },{
+        key: 'G',
         sprite: 'G',
         type: 'big-gold',
         description: 'a large gold coin'
     },{
+        key: 'i',
         sprite: 'i',
         type: 'random-normal-item',
         description: 'this normal looking item needs to be identified.'
     },{
+        key: 'I',
         sprite: 'I',
         type: 'random-rare-item',
         description: 'a rare item that must be identified'
@@ -54,10 +205,32 @@ var player = {
     x: 0,
     y: 0,
     gold: 0,
+    level: 0,
+    name: 'player1',
     inventory: [],
     leftHand: false,
     rightHand: false
 };
+
+function drawHeader(player, header) {
+    header.innerHTML = "<span>name: " + player.name + " gold: " + player.gold + "</span>"
+}
+
+function drawFooter(player, footer) {
+    var items = "["
+    footer.innerHTML = "<span> items: "
+
+    for(var i = 0; i < maxItems; i++) {
+        if (player.inventory.length > i) {
+            items += player.inventory[i].key + ",";
+        } else {
+            items += "  ";
+        }
+    }
+
+    items += "]";
+    footer.innerHTML += items + "</span>"
+}
 
 function drawDungeon(inMap, inPlayer, inDungeon) {
     var dungeonMap = "";
@@ -104,29 +277,71 @@ function boundsCheck(map, posx, posy) {
     return false;
 }
 
-function canTileBeMovedOver(map, posx, posy) {
+function canTileBeMovedOver(map, player, posx, posy) {
     if (boundsCheck(map, posx, posy)) {
-        return map.data[posy].charAt(posx) !== map.wall;
+        var tile = map.data[posy].charAt(posx);
+        console.log(tile);
+        if (tile !== map.wall && tile !== map.closedDoor) {
+            return true;
+        }
+
+        if (tile === map.closedDoor) {
+            var keyIndex = -1;
+            for(var i = 0; i < player.inventory.length; i++) {
+                if (player.inventory[i].key === 'k') {
+                    keyIndex = i;
+                    break;
+                }
+            }
+
+            if (keyIndex > -1) {
+                player.inventory.splice(keyIndex, 1);
+                return true;
+            }
+        }
     }
 
     return false;
 }
 
+function getItemFromTile(tile, items) {
+    if (tile === 'i') {
+        // randomly get an item from normal items
+        var index = (Math.random() * 1000) + 1;
+        index = Math.ceil(index);
+        index = index % normal_item_types.length;
+        return normal_item_types[index];
+    } else if (tile === 'I') {
+        // randomly get an item from rare items
+    } else {
+        var item = undefined;
+        for(var i = 0; i < items.length; i++) {
+            if (items[i].key === tile) {
+                item = items[i];
+            }
+        }
+
+        return item;
+    }
+}
+
 function lookForItem(map, player, posx, posy) {
-    var tile = map.data[posy].charAt(posx);
+    var row = map.data[posy];
+    var tile = row.charAt(posx);
     if (tile !== '.') {
         if (tile === 'g') {
-            // GOLD!!
-
+            player.gold += 10;
         } else if (tile === 'G') {
-
-        } else if (tile === 'k') {
-
-        } else if (tile === 'i') {
-
-        } else if (tile === 'I') {
-
+            player.gold += 100;
+        } else {
+            var item = getItemFromTile(tile, ground_item_types);
+            if (item) {
+                player.inventory.push(item);
+            }
         }
+
+        row = stringReplace(row, tile, ".", posx - 1, posx + 1);
+        map.data[posy] = row;
     }
 }
 
@@ -136,7 +351,9 @@ function movePlayer(map, player, posx, posy) {
     player.x = posx;
     player.y = posy;
 
+    drawHeader(player, header);
     drawDungeon(map.data, player, dungeon);
+    drawFooter(player, footer);
 }
 
 function characterMovement(map, player, code) {
@@ -154,7 +371,7 @@ function characterMovement(map, player, code) {
         newx = newx + 1;
     }
 
-    if (canTileBeMovedOver(map, newx, newy)) {
+    if (canTileBeMovedOver(map, player, newx, newy)) {
         movePlayer(map, player, newx, newy);
     }
 }
@@ -168,11 +385,42 @@ function keypress(event) {
     }
 }
 
+function stringReplace(original, pattern, replacement, start, end) {
+    var front = "",
+        middle = "",
+        tail = "";
+
+    if (start > 0) {
+        front = original.slice(0,start);
+    }
+
+    if (end < original.length - 1) {
+        tail = original.slice(end, original.length);
+    }
+
+    if (start > 0 || end < original.length) {
+        middle = original.slice(start, end);
+    } else {
+        middle = original;
+    }
+
+    middle = middle.replace(pattern, replacement);
+
+    console.log(front);
+    console.log(middle);
+    console.log(tail);
+
+    return front + middle + tail;
+}
+
+
+
 function initialize() {
   console.log('initialize...');
   dungeon = document.getElementById('dungeon');
   header = document.getElementById('header');
   footer = document.getElementById('footer');
+  maxItems = 5;
   
   player.x = 1;
   player.y = 1;
@@ -183,6 +431,8 @@ function initialize() {
 
   document.onkeydown = keypress
 
+  drawHeader(player, header);
+  drawFooter(player, footer);
   drawDungeon(map.data, player, dungeon);
 };
 
