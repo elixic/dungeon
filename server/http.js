b3c8fd6976f244327ce78e27d944922363e0aea7
Eian <eiancoun@gmail.com>
2014-05-18 02:25:57 -0400

    hook up socket.io

--- server/http.js
+++ server/http.js
@@ -1,11 +1,17 @@
 var log = require("./logging"); 
 var express = require("express");
+var socketio = require('socket.io');
 
 
 
 function HTTPServer(world) {
   this.http = express(); // create express app
   this.world = world; // link to world
+
+  // hook up socket.io...
+  // TODO: when we move to Express v4 we need to update how we hook up express
+  var server = require('http').createServer(express);
+  io = socketio.listen(server);
 
   // initialize middleware to support sessions
   this.http.use(express.cookieParser());
