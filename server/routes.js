ce07771bdb66facc2944968d6f36cfbbf258a88c
Eian <eiancoun@gmail.com>
2014-05-18 01:54:35 -0400

    serve status pages under public/

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 var log = require("./logging");
 var handlers = require("./requestHandlers");
+var express = require('express');
 
 module.exports = function(httpserv){
+
+  httpserv.use(express.static(__dirname + '/public'));
 
   httpserv.get('/roomMap', handlers.roomMap);
   httpserv.get('/login', handlers.login);
